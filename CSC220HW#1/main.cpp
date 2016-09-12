f5daeb2a5c5702d8748c99c95d61fb5449cf0ff2
Shirong Zheng <shirongzheng@shirongdemacbook-pro.local>
2016-09-12 10:29:12 -0400

    Initial Commit

---
+++ CSC220HW#1/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  CSC220HW#1
+//
+//  Created by Shirong Zheng on 16/9/12.
+//  Copyright © 2016年 Shirong Zheng. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
